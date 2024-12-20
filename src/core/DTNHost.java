6a921f8abda74071622fb08b4dfa55231174941e
StackOverfloweds <bryangithub32@gmail.com>
2024-12-20 12:31:13 +0700

    update: add initital for pubs-subs

--- src/core/DTNHost.java
+++ src/core/DTNHost.java
@@ -5,13 +5,10 @@
 package core;
 
 import java.util.*;
-
 import movement.MovementModel;
 import movement.Path;
 import routing.MessageRouter;
 import routing.RoutingInfo;
-
-// tambahan
 import routing.community.Duration;
 
 /**
@@ -51,6 +48,14 @@ public class DTNHost implements Comparable<DTNHost> {
     public Map<DTNHost, Duration> durPerNode;
     public Map<DTNHost, List<Duration>> listDurPerNode;
     public double totalContactTime = 0;
+
+    // crete variable for pubs-susb
+    private boolean isPublisher = false;
+    private boolean isBroker = false;
+    private boolean isSubscriber = false;
+
+    private List<Double> interest;
+    private List<Boolean> ownInterest;
         
     static {
         DTNSim.registerForReset(DTNHost.class.getCanonicalName());
@@ -76,8 +81,46 @@ public class DTNHost implements Comparable<DTNHost> {
         this.comBus = comBus;
         this.location = new Coord(0, 0);
         this.address = getNextAddress();
-        this.name = groupId + address;
         this.net = new ArrayList<NetworkInterface>();
+
+        // create initial node
+        this.isPublisher = false;
+        this.isSubscriber = false;
+        this.isBroker = false;
+
+        // create role for pubs-subs with group
+
+        if (groupId.startsWith("S")) {
+            isSubscriber = true;
+            name = "Subscriber_" + groupId + "_" +address;
+        } else if (groupId.startsWith("P")) {
+            isPublisher = true;
+            name = "Publisher_" + groupId + "_" + address;
+        } else if (groupId.startsWith("B")) {
+            isBroker = true;
+            name = "Broker_" + groupId + "_" + address;
+        } else {
+            throw new IllegalArgumentException("Invalid Group ID");
+        }
+
+        // create subscriber interest for something random
+        if (isSubscriber) {
+            Random random = new Random();
+            ownInterest = new ArrayList<Boolean>();
+            interest = new ArrayList<Double>();
+
+            // just set random value for interest subsriber
+            double numInterest = 0.0 +random.nextDouble(1.0);
+
+            // looping for the interest
+            while (numInterest > 0.0) {
+                double interestValue = random.nextDouble();
+                interest.add(interestValue);
+                ownInterest.add(true);
+                numInterest--;
+            }
+
+        }       
 
         for (NetworkInterface i : interf) {
             NetworkInterface ni = i.replicate();
